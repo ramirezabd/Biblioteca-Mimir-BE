7a35ef1a5434ed40beef754a0fe32f1cd7282158
Fajar Abdurrahman <kamentosa@gmail.com>
2023-01-04 14:07:46 +0700

    still figuring out the access and refresh token problem

--- src/controllers/userToken.js
+++ src/controllers/userToken.js
@@ -37,34 +37,74 @@ const Register = async (req, res) => {
 
 const Login = async (req, res) => {
     try {
+        console.log(" ")
         const user = await anggota.findAll({
             where: {
                 username: req.body.username
             }
         });
+
+        console.log(user)
+
         const match = await bcrypt.compare(req.body.password, user[0].password);
-        if (!match) return res.status(400).json({ msg: "Wrong Password" });
+        if (!match) return res.status(400).json({ msg: "passwordnya salah mang" });
+
+
+        console.log(" ");
+        console.log(`password pre: ` + req.body.password);
+        console.log(`password post: ` + user[0].password)
+        console.log(`is it match? ` + match)
+        console.log(" ");
+
+
         const userId = user[0].id;
         const name = user[0].namaAnggota;
         const username = user[0].username;
-        const accessToken = jwt.sign({ userId, name, username }, process.env.ACCESS_TOKEN_SECRET, {
-            expiresIn: '20s'
-        });
-        const refreshToken = jwt.sign({ userId, name, username }, process.env.REFRESH_TOKEN_SECRET, {
-            expiresIn: '1d'
-        });
-        await anggota.update({ refresh_token: refreshToken }, {
-            where: {
-                id: userId
-            }
-        });
-        res.cookie('refreshToken', refreshToken, {
-            httpOnly: true,
-            maxAge: 24 * 60 * 60 * 1000
-        });
-        res.json({ accessToken });
+
+
+        console.log(" ");
+        console.log(`ID: ` + userId);
+        console.log(`name: ` + name);
+        console.log(`username: ` + username);
+        console.log(" ")
+
+
+        // const accessToken = jwt.sign({
+        //     userId: anggota[0].id,
+        //     name: anggota[0].namaAnggota,
+        //     username: anggota[0].username
+        // }, process.env.ACCESS_TOKEN_SECRET, {
+        //     expiresIn: '20s'
+        // });
+
+        // const refreshToken = jwt.sign({ userId, name, username }, process.env.REFRESH_TOKEN_SECRET, {
+        //     expiresIn: '1d'
+        // });
+
+
+        // console.log(" ")
+        // console.log("kenapa ga keliatan access sama refreshnya")
+        // console.log(`access token: ` + accessToken)
+        // console.log(`refresh token: ` + refreshToken)
+        // console.log(" ")
+
+
+        // await anggota.update({ refresh_token: refreshToken }, {
+        //     where: {
+        //         id: userId
+        //     }
+        // });
+        // res.cookie('refreshToken', refreshToken, {
+        //     httpOnly: true,
+        //     maxAge: 24 * 60 * 60 * 1000
+        // });
+        // res.json({ accessToken });
+
+        console.log("Berhasil")
+        
     } catch (error) {
         res.status(404).json({ msg: "username atau password tidak ditemukan" });
+        console.log("Tidak ditemukan, tapi versi console")
     }
 }
 
--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -2,6 +2,9 @@ const jwt = require("jsonwebtoken");
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
+
+    console.log(`authHeader status : `+authHeader)
+
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401);
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
